e22b40b9e083a7d856ab21ea6091a27f0f261a3c
Fuad Khan <khan35-883@diu.edu.bd>
2025-02-15 00:18:43 +0600

    about routes/web.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,3 +1,5 @@
 <h1>Hi, It's MK</h1>
 
 <a href="/post">Post page</a>
+<a href="/about">About page</a>
+<a href="/contact">Contact page</a>
--- routes/web.php
+++ routes/web.php
@@ -6,10 +6,28 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/about', function () {
+    return view('about');
+});
+
 Route::get('/post', function () {
     return view('post');
 });
 
+
+
 Route::get('/post/firstpost', function () {
     return view('firstpost');
 });
+
+Route::get('/post/{id?}', function (string $id = null) {
+    if($id) {
+        return "<h1>Post ID : ". $id. "</h1>";
+        
+    }else{
+       
+        return "<h1>No post ID provided</h1>";
+    }
+    
+})->whereAlphaNumeric('id');
+
